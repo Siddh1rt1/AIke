75c474e4a9e833620770b48f67092e8b1cf5a201
Siddh1rt1 <93163996+Siddh1rt1@users.noreply.github.com>
2021-11-17 22:42:27 +0100

    Delete cgi-bin directory

--- cgi-bin/best.py
+++
@@ -1,7 +0,0 @@
-
-import js2py
-js1 = 'var x = /data/index.getElementById("story").value ; console.log("Hello World")'
-js2 ='var x = document.getElementById("story").value' 
-js3 = ' document.getElementById("story").value= x + "textmore" '
-res1 = js2py.eval_js(js1)
-res1
