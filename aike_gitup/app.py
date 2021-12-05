02235cde9d2823975581d7b08e0c69db614816d9
Siddh1rt1 <93163996+Siddh1rt1@users.noreply.github.com>
2021-12-05 20:48:43 +0100

    Add files via upload

--- aike_gitup/app.py
+++ aike_gitup/app.py
@@ -5,6 +5,7 @@ import os, random
 import jinja2
 import argparse
 import re
+import time
 
 SenCut = True
 global_input = "Merz"
@@ -17,6 +18,7 @@ check_4=0
 option="normal"
 option2="send"
 classic_var=0
+status = "html"
 
 @app.route('/')
 def index():
@@ -24,9 +26,33 @@ def index():
   check_1="checked"
   return render_template('index.html',val1="Merz", val2="10", timer=global_timer, timer_variable=global_timer, input_variable = global_input, words_variable = global_words, checked1="checked", checked2=check_2, checked3=check_3, checked4=check_4)
 
+@app.route('/_stuff', methods= ['GET'])
+def stuff():
+    cpu=round(getCpuLoad())
+    ram=round(getVmem())
+    disk=round(getDisk())
+    return jsonify(cpu=cpu, ram=ram, disk=disk)
+
+@app.route('/background_process_test', methods= ['GET', 'POST'])
+def setstatusold():
+    status= "statusisserver"
+    return status
+
+
+
+@app.route('/getStatus')
+def get_status():
+    global status
+    batus=status
+    return jsonify(batus)
+
+
+
 @app.route('/', methods=['POST'])
 def my_form_post():
 
+    global status
+    status="server"
     in_text = request.form['text']
     global global_input
     global_input=str(in_text)
@@ -34,7 +60,9 @@ def my_form_post():
     in_words = request.form['text3']
     global global_words 
     global_words= int(in_words)
-    
+    status = "train"
+   
+
     global option
     option = request.form['radio']
 
@@ -83,7 +111,9 @@ def my_form_post():
      
       out_result=('')
       x_ticker=int(timer)
-      
+      status = "train"
+      num=1
+      num=num-1
       for x in range(x_ticker):
         print (x)
         x=x+1
@@ -91,17 +121,17 @@ def my_form_post():
         from train import dataset, model, predict 
         #train.predict(dataset,model,text)
         #besult= train.train(dataset,model,args)
-        text_result= train.predict(option ,dataset , model, in_text, next_words=global_words)
+        text_result= train.predict(option ,dataset , model, in_text, next_words=global_words) 
         print(text_result)
 
         if option in "Aggro":
           text_result = text_result.upper()
         else:
           print("noupper")
-        
-        out_result=out_result+text_result+" \n"
-
-    return render_template('index.html',data=out_result+str(global_timer), timer_variable=global_timer, input_variable = global_input, words_variable = global_words, checked1=check_1, checked2=check_2, checked3=check_3, checked4=check_4)
+        num=num+1
+        out_result=out_result+"["+str(num)+"] "+text_result+" \n"
+    status="html"
+    return render_template('index.html',data=out_result, timer_variable=global_timer, input_variable = global_input, words_variable = global_words, checked1=check_1, checked2=check_2, checked3=check_3, checked4=check_4)
 
 def checkboxes():
    print("yes")   
